e347b3475cd690ac6b8f1f823c1478fda2b0b45c
Alessia Trianti <alestrianti+github@gmail.com>
2025-04-02 22:40:29 +0200

    Update client-server config (use proxie)

--- server/server.js
+++ server/server.js
@@ -9,12 +9,6 @@ connectDB();
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Write .env file in client/
-fs.writeFileSync('../client/.env', `VITE_API_URL=http://localhost:${port}`);
-
-app.use(cors({
-    origin: 'http://localhost:5173', // allow Vite dev server
-}));
 app.use(express.json()); // body parser
 app.use(errorHandler);
 
